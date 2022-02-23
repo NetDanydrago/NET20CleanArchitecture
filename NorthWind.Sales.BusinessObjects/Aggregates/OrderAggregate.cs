3ebced92a67256ad9ecec9e3f9dcbf67c4cbac42
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2022-02-22 21:33:37 -0600

    se agrega OrderAggregates

---
+++ NorthWind.Sales.BusinessObjects/Aggregates/OrderAggregate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.BusinessObjects.Aggregates
+{
+    public class OrderAggregate : Order
+    {
+        readonly List<OrderDetail> OrderDetailsField = new();
+        public IReadOnlyCollection<OrderDetail> OrderDetails => OrderDetailsField;
+
+        public void AddDetail(OrderDetail orderDetail)
+        {
+            var ExistingOrderDetail = OrderDetailsField.FirstOrDefault
+                (o => o.ProductId == orderDetail.ProductId);
+            if(ExistingOrderDetail != default)
+            {
+                OrderDetailsField.Add(ExistingOrderDetail with 
+                { Quantity = (short)(orderDetail.Quantity + ExistingOrderDetail.Quantity)});
+                OrderDetailsField.Remove(ExistingOrderDetail);
+            }
+            else
+            {
+                OrderDetailsField.Add(orderDetail);
+            }
+
+        }
+
+        public void AddDetail(int productId, decimal unitPrice,short quantity) => 
+            AddDetail(new OrderDetail(productId, unitPrice, quantity));
+    }
+}
