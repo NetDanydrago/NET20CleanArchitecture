b10b7b179ba7d70b3010b7578c6ca5ccfa8cab25
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2022-02-24 20:02:57 -0600

    detail configuration

---
+++ NorthWindEFCore.Repositories/Configurations/OrderConfiguration.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindEFCore.Repositories.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(o => o.CustomerId)
+                .IsRequired()
+                .HasMaxLength(5)
+                .IsFixedLength();
+            builder.Property(o => o.ShipAddress)
+                .IsRequired()
+                .HasMaxLength(60);
+            builder.Property(o => o.ShipCity)
+                .HasMaxLength(15);
+            builder.Property(o => o.ShipCountry).
+                HasMaxLength(15);
+            builder.Property(o => o.ShipPostalCode)
+                .HasMaxLength(10);
+        }
+    }
+}
--- NorthWindEFCore.Repositories/_Imports.cs
+++ NorthWindEFCore.Repositories/_Imports.cs
@@ -1,1 +1,4 @@
-﻿global using NorthWind.Sales.BusinessObjects.POCOEntities; 
+﻿global using NorthWind.Sales.BusinessObjects.POCOEntities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using NorthWindEFCore.Repositories.Entities;
